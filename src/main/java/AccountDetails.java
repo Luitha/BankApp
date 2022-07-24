0b9372580781a1046b77fd8c8f7446408fc272cb
Luitha <bardat.87@gmail.com>
2022-07-24 13:20:25 +0200

    Tworzenie aplikacji bankowej

---
+++ src/main/java/AccountDetails.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class AccountDetails {
+    private String name;
+    private String accountNumber;
+    private String login;
+    private long balance;
+
+    public void showAccount() {
+        System.out.println("Your account name is " + name);
+        System.out.println("Your account number is " + accountNumber);
+        System.out.println("Your login is " + login);
+        System.out.println("Your account balance is " + balance);
+    }
+
+    Scanner scanner = new Scanner(System.in);
+
+    public void createAccount() {
+        System.out.println("Provide account name ");
+        name = scanner.next();
+        System.out.println("Provide account number ");
+        accountNumber = scanner.next();
+        System.out.println("Provide your login ");
+        login = scanner.next();
+        System.out.println("Provide your account balance ");
+        balance = scanner.nextLong();
+    }
+}
